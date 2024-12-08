4c9c84cd76ed32769be12fa552bbd6e75e1c76db
Alexander Profeit <alex@profeit.com>
2024-12-07 22:14:38 -0500

    add dampened_safe?

--- day_2/report.rb
+++ day_2/report.rb
@@ -6,13 +6,30 @@ class Report
   end
 
   def safe?
-    ascending = levels.each_cons(2).all? { |a, b| a < b }
-    descending = levels.each_cons(2).all? { |a, b| a > b }
     safe_deltas = levels.each_cons(2).all? do |a, b|
       delta = (b - a).abs
       delta >= 1 && delta <= 3
     end
 
-    (ascending || descending) && safe_deltas
+    (ascending? || descending?) && safe_deltas
+  end
+
+  def dampened_safe?
+    levels.each_index do |i|
+      removed_self = levels[0...i] + levels[i+1..-1]
+      report = Report.new(removed_self)
+
+      return true if report.safe?
+    end
+
+    false
+  end
+
+  def ascending?
+    levels.each_cons(2).all? { |a, b| a < b }
+  end
+
+  def descending?
+    levels.each_cons(2).all? { |a, b| a > b }
   end
 end
--- day_2/report_test.rb
+++ day_2/report_test.rb
@@ -19,4 +19,22 @@ class ReportTest < Minitest::Test
 
     assert_equal false, report.safe?
   end
+
+  def test_safe_dampened_safe_without_removing_element
+    report = Report.new([7, 6, 4, 2, 1])
+
+    assert_equal true, report.dampened_safe?
+  end
+
+  def test_unsafe_dampened_safe
+    report = Report.new([1, 2, 7, 8, 9])
+
+    assert_equal false, report.dampened_safe?
+  end
+
+  def test_safe_dampened_safe_removing_element
+    report = Report.new([8, 6, 4, 4, 1])
+
+    assert_equal true, report.dampened_safe?
+  end
 end
